a92e5f69e13051be0750fbc934ca868129167921
Fennec Border <me@flolon.cc>
2025-04-19 16:02:13 -0400

    turn post block into PHP object

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require('src/API.php');
+require('src/PostBlock.php');
 
 use STiBaRC\STiBaRC;
 
@@ -22,24 +23,10 @@ $api = new STiBaRC\API("development", true);
 
     <div class="announcement"><?= $api->getAnnouncement(); ?></div>
 
-    <?php foreach ($api->getPosts() as $post) {
-        $poster = $post->poster;
-        $date = strtotime($post->date);
-    ?>
-        <div class="postblock">
-            <div class="title"><?= $post->title ?></div>
-            <a class="userlink" title="<?= $poster->verified ? "Verified" : $poster->username ?>">
-                <img class="pfp" width="25px" src="<?= $poster->pfp ?>">
-                <span class="username">
-                    <?= $poster->username ?>
-                    <span class="pronouns"><?= $poster->pronouns ? '(' . $poster->pronouns . ')' : "" ?></span>
-                </span>
-            </a>
-            <div class="date" title="<?= $post->date ?>">
-                <?= date("m/d/y h:i:s A", $date) ?>
-            </div>
-        </div>
-    <?php } ?>
+    <?php foreach ($api->getPosts() as $postData) {
+        $postHtml = new STiBaRC\PostBlock($postData);
+        echo $postHtml->post();
+     } ?>
 
 </body>
 
---
+++ src/PostBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace STiBaRC\STiBaRC;
+
+class PostBlock
+{
+
+    public $post;
+
+    public function __construct($postData)
+    {
+        $this->post = $postData;
+    }
+
+    public function post()
+    {
+
+        $poster = $this->post->poster;
+        $date = strtotime($this->post->date);
+
+        return '
+        <div class="postblock">
+            <div class="title">' . $this->post->title . '</div>
+            <a class="userlink" title="' . ($poster->verified ? "Verified" : $poster->username) . '">
+                <img class="pfp" width="25px" src="' . $poster->pfp . '">
+                <span class="username">' . $poster->username . '
+                <span class="pronouns">' . ($poster->pronouns ? '(' . $poster->pronouns . ')' : "") . '</span>
+                </span>
+            </a>
+            <div class="date" title="' . $this->post->date . '">
+                ' . date("m/d/y h:i:s A", $date) . '
+            </div>
+        </div>';
+    }
+}
