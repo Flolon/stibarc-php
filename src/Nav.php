3d95e33db54f67319a62885da886dc7042cabec5
Sophie <me@flolon.cc>
2026-03-02 21:54:25 -0500

    add logged in pfp & username in nav

--- src/Nav.php
+++ src/Nav.php
@@ -16,24 +16,29 @@ class Nav
 	{
 		$navHTML = '
         <nav class="mainNav">
-          <ul>
-		  	<li class="logo"><a href="./"><img src="./img/logo_32.png" height="24px" alt="STiBaRC Home"></a></li>
-        	<li>
-				<form action="search.php">
+        	<ul>
+				<li class="logo"><a href="./"><img src="./img/logo_32.png" height="24px" alt="STiBaRC Home"></a></li>
+        		<li>
+					<form action="search.php">
     	    	    <input type="search" name="q" placeholder="Search" value="'
-			. ($this->searchQuery ? $this->searchQuery : '') . '">
+						. ($this->searchQuery ? $this->searchQuery : '') . '">
 	            	<button type="submit">Search</button>
         		</form>
-			</li>
-			';
+				</li>
+			</ul>
+			<ul class="right">';
 		if (empty($_SESSION['sess'])) {
 			$navHTML .= '<li><a href="./login.php">Login</a></li>';
 		} else {
-			$navHTML .= '<li class="right"><a href="./logout.php" title="Logged in as ' . $_SESSION['username'] . '">Logout</a></li>';
+			$navHTML .= '<li title="Logged in as ' 
+				. htmlspecialchars($_SESSION['username']) . '"><img class="pfp" src="'
+				. $_SESSION['pfp'] . '" height="24px alt="Pfp"><span style="vertical-align: middle;">'
+				. htmlspecialchars($_SESSION['username']) . '</li>
+			<li><a href="./logout.php">Logout</a></li>';
 		}
 		$navHTML .= '
-          </ul>
-        </nav>';
+			</ul>
+		</nav>';
 		return $navHTML;
 	}
 }
