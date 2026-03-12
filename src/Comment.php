470ddcd728b90b61bb668328a6f9ce07446dbafa
Sophie <me@flolon.cc>
2026-03-12 16:51:24 -0400

    date does not need to be a block (title)

--- src/Comment.php
+++ src/Comment.php
@@ -23,19 +23,21 @@ class Comment
 
 		$commentHTML = '
 		<div class="commentBlock" id="comment-' . $this->comment->id . '">
-			<a class="userlink" href="user.php?username=' . htmlspecialchars($poster->username) . '" 
-			title="' . htmlspecialchars($poster->username) . '">
-				<img class="pfp" width="30px" src="' . $poster->pfp . '">
-				<span class="username">' . htmlspecialchars($poster->username) . '</span>
-				' . ($poster->verified ? '<span class="verified" title="Verified user">
-				<img class="icon" src="./img/icon/verified.png" height="14px" alt="Verified"></span>' : '') . '
-			</a>
-			<span class="pronouns" title="Pronouns">
-				' . ($poster->displayPronouns && $poster->pronouns ? '(' . htmlspecialchars($poster->pronouns) . ')' : "") . '
-			</span>
-			<div class="date" title="' . $this->comment->date . '">
-				' . date("m/d/y, g:i A", $date) . '
+			<div>
+				<a class="userlink" href="user.php?username=' . htmlspecialchars($poster->username) . '" 
+				title="' . htmlspecialchars($poster->username) . '">
+					<img class="pfp" width="30px" src="' . $poster->pfp . '">
+					<span class="username">' . htmlspecialchars($poster->username) . '</span>
+					' . ($poster->verified ? '<span class="verified" title="Verified user">
+					<img class="icon" src="./img/icon/verified.png" height="14px" alt="Verified"></span>' : '') . '
+				</a>
+				<span class="pronouns" title="Pronouns">
+					' . ($poster->displayPronouns && $poster->pronouns ? '(' . htmlspecialchars($poster->pronouns) . ')' : "") . '
+				</span>
 			</div>
+			<span class="date" title="' . $this->comment->date . '">
+				' . date("m/d/y, g:i A", $date) . '
+			</span>
 			<hr>
 			<div class="content">' . htmlspecialchars($this->comment->content) . '</div>';
 
